e1dd3e901d521ddd3757853cfabbc61ea43851f9
Esther Hersh <estherhersh@172-17-39-172.DYNAPOOL.NYU.EDU>
2017-11-09 12:32:35 -0500

    fixes #119 update

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^accounts/password/reset/$', views.edit_profile, name='edit_profile')
+    # url(r'^accounts/password/reset/$', views.edit_profile, name='edit_profile')
 ]
