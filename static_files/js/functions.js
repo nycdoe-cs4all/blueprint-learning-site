140186f5a6f7ce59118cb739b11740f6e9a1b598
aankit <aankit.patel@gmail.com>
2018-03-10 15:07:01 -0500

    phase 2 style changes - about, home, base, outcomes, resources

--- static_files/js/functions.js
+++ static_files/js/functions.js
@@ -1,7 +1,7 @@
 // Javascript functions
 
-
-
+    
+    
 //scroll functions
 $.fn.scrollView = function () {
     return this.each(function () {
@@ -15,81 +15,139 @@ $.fn.scrollView = function () {
 
 // Run on Document Ready
 $( document ).ready(function() {
-
-
-
+    
+    
+    
     // Colorbox
     $('a.embed').each(function() {
-    	vidTitle = $(this).attr('data-title');
-    	vidSubtitle = $(this).attr('data-subtitle');
-    	if ( vidSubtitle ) {
-    		vidTitle = vidTitle + ' <span class="subtitle">' + vidSubtitle + '</span>';
-    	}
-    	$(this).colorbox({iframe:true, transition:'fade', innerWidth:1920, innerHeight:1080, maxWidth:'100%', maxHeight:'100%', title: vidTitle, opacity: 1 });
+        vidTitle = $(this).attr('data-title');
+        vidSubtitle = $(this).attr('data-subtitle');
+        if ( vidSubtitle ) {
+            vidTitle = vidTitle + ' <span class="subtitle">' + vidSubtitle + '</span>';
+        }
+        $(this).colorbox({iframe:true, transition:'fade', innerWidth:1920, innerHeight:1080, maxWidth:'100%', maxHeight:'100%', title: vidTitle, opacity: 1 });
     });
-
-
+    
+        
+    $('a.toggle').click(function(event) {
+        event.preventDefault();
+        targetDiv = $(this).attr('href');
+        $(targetDiv).toggleClass('active');
+    });
+    
+    
     $('a.accordion').click(function(event) {
-    	event.preventDefault();
-    	targetDiv = $(this).attr('href');
-    	$(targetDiv).siblings().addClass('compressed');
-    	$(targetDiv).toggleClass('compressed');
-    	if ( !$(targetDiv).hasClass('compressed') ) {
-    		setTimeout(function() {
-    			$(targetDiv).scrollView();
-    		}, 500);
-    	} else {
-
-    	}
-    });
-
-
+            event.preventDefault();
+            targetDiv = $(this).attr('href');
+            $(targetDiv).siblings().addClass('compressed');
+            $(targetDiv).toggleClass('compressed');
+            if ( !$(targetDiv).hasClass('compressed') ) {
+                setTimeout(function() {
+                    $(targetDiv).scrollView();
+                }, 500);
+            } else {
+    
+            }
+        });
+    
+    
     $('a.scroll').not('.accordion').click(function(event) {
-    	event.preventDefault();
-    	targetDiv = $(this).attr('href');
-    	$(targetDiv).scrollView();
+        event.preventDefault();
+        targetDiv = $(this).attr('href');
+        $(targetDiv).scrollView();
     });
+    
+    
 
-
-
+    
+    
+    
+    $('td').hover(function() {
+        targetTh = $(this).attr('data-header');
+        $('#' + targetTh).addClass('current');
+    }, function() {
+        targetTh = $(this).attr('data-header');
+        $('#' + targetTh).removeClass('current');
+    });
+    
+    
+    
     // Change things when scrolling begins
 //    var waypoints = $(window).waypoint({
-//    	handler: function(direction) {
-//    		if (direction == 'down') {
-//    			$("body").addClass('scrolling');
-//        	} else {
-//    			$("body").removeClass('scrolling');
-//    		}
-//    	},
-//    	offset: '-60'
+//      handler: function(direction) {
+//          if (direction == 'down') {
+//              $("body").addClass('scrolling');
+//          } else {
+//              $("body").removeClass('scrolling');
+//          }
+//      },
+//      offset: '-60'
 //    })
-
-
-
+    
+    
+    
     // Determine whether or not to show intro animation
 //    animation = $.cookie('animation');
 //    if ( animation == 'viewed' ) {
-//    	$('body').removeClass('animation');
+//      $('body').removeClass('animation');
 //    }
-
-
-
+    
+    
+    
 });
 
 
 
 // Run on load
-$(window).on("load", function (e) {
-
-	$('body').removeClass('preload');
-
-//	$.cookie('animation', 'viewed', { expires: 7, path: '/' }); // Set a cookie to indicate animation has been viewed
+$(window).load(function() {
+    
+    $('#menu .site-menu li ul').each(function() {
+        submenuHeight = $(this).height();
+        $(this).css('height', submenuHeight);
+        $(this).parent('li').addClass('compressed parent-menu');        
+    });
+    $('.parent-menu > a').click(function(event) {
+        event.preventDefault();
+        $(this).parent('li').toggleClass('compressed');
+    });
+    
+    $('#page-menu .options').each(function() {
+        submenuHeight = $(this).height();
+        $(this).css('height', submenuHeight);
+        $(this).parent('fieldset').addClass('compressed');      
+    });
+    $('#page-menu legend a').click(function(event) {
+        event.preventDefault();
+        $(this).closest('fieldset').toggleClass('compressed');
+    });
+    
+    $('.article-content .read-more').each(function() {
+        submenuHeight = $(this).height();
+        $(this).css('height', submenuHeight);
+        $(this).parent('.article-content').addClass('compressed');      
+    });
+    $('.article-content a.open').click(function(event) {
+        event.preventDefault();
+        $(this).closest('.article-content').toggleClass('compressed');
+    });
+        
+    $('body').removeClass('preload');
+    
+//  $.cookie('animation', 'viewed', { expires: 7, path: '/' }); // Set a cookie to indicate animation has been viewed
 
     $('.accordion-target .section-content').each(function() {
-    	divHeight = $(this).height();
-    	$(this).css('height', divHeight);
-		$(this).parent('.accordion-target').addClass('compressed');
+        divHeight = $(this).height();
+        $(this).css('height', divHeight);
+        $(this).parent('.accordion-target').addClass('compressed');
     });
+    if (window.location.hash) {
+        var hash = document.URL.substr(document.URL.indexOf('#')+1);
+        $('#' + hash).removeClass('compressed');
+        setTimeout(function() {
+            $('#' + hash).scrollView();
+        }, 500);
+    }
+    
 
 
 });
@@ -98,7 +156,7 @@ $(window).on("load", function (e) {
 
 // Run on Ajax Complete
 $( document ).ajaxComplete(function( event, request, settings ) {
-
+      
 });
 
 
@@ -107,24 +165,26 @@ var resizeTimer;
 
 $(window).on('resize', function(e) {
 
-	$('.accordion-target .section-content').css('height', '');
+    $('.accordion-target .section-content').css('height', '');
+    
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function() {
 
-	clearTimeout(resizeTimer);
-	resizeTimer = setTimeout(function() {
-
-		$('.accordion-target .section-content').each(function() {
-			var reclose = false;
-			if ( $(this).parent('.accordion-target').hasClass('compressed') ) {
-				$(this).parent('.accordion-target').removeClass('compressed');
-				reclose = true;
-			}
-			divHeight = $(this).height();
-			$(this).css('height', divHeight);
-			if ( reclose == true ) {
-				$(this).parent('.accordion-target').addClass('compressed');
-			}
-		});
-
-	}, 250);
+        $('.accordion-target .section-content').each(function() {
+            var reclose = false;
+            if ( $(this).parent('.accordion-target').hasClass('compressed') ) {
+                $(this).parent('.accordion-target').removeClass('compressed');
+                reclose = true;
+            }
+            divHeight = $(this).height();
+            $(this).css('height', divHeight);
+            if ( reclose == true ) {
+                $(this).parent('.accordion-target').addClass('compressed');
+            }
+        });
+        
+    }, 250);
 
 });
+
+
--- static_files/js/ican.js
+++ static_files/js/ican.js
@@ -8,7 +8,7 @@ var options={
 	,"z":"Citizen"
 	,"an":"Analyze"
 	,"co":"Communicate"
-	,"pr":"Practice"
+	,"pr":"Prototype"
 	,"ab":"Abstraction"
 	,"al":"Algorithms"
 	,"p":"Programming"
@@ -42,17 +42,27 @@ var subcons={
 };
 var	theurl="http://cs4all.nyc/api/ican";
 function fetchStatement() {
+
 	//Empty table before we append to it
-	$("#userdata tbody").empty();
+	$('#userdata tbody').empty();
+	$('#i-can-results').addClass('hidden');
+	
 	//Dont fetch data until user has chosen all options
 	if(perSelect.localeCompare("null")!=0&&prcSelect.localeCompare("null")!=0&&conSelect.localeCompare("null")!=0){
+		
+		var tableData = '';
+		
 		// Load loader circle
 		$("#loaderContainer").html("<div class=\"loader\" style=\"margin:auto;\"></div>");
+		
 		$.getJSON(theurl, function(data) {
+		
 			// get rid of loader once the info has been fetched
-			$("#loaderContainer").html("<div></div>");
+			$('#loaderContainer').html('');
+			
 			//reference variable to see if a new subconcept header is needed
 			var ref="";
+			
 			//loop through JSON, f==object
 			$.each(data, function(i, f) {
 				//format codification for each json object to compare to the selected options
@@ -62,67 +72,89 @@ function fetchStatement() {
 				per=per.toLowerCase();
 				con=con.toLowerCase();
 				prc=prc.toLowerCase();
+				
 				//if object matches Perspective, Practive and Concept
-				if(perSelect.localeCompare(per)==0){
-					if(prcSelect.localeCompare(prc)==0){
-						if(conSelect.localeCompare(con)==0){
-							$(".tblinfo").html("These are the I can statements for your selection of "+options[perSelect]+", "+options[prcSelect]+" and "+options[conSelect]);
-							//unhide table
-							$(".tblinfo").css("display","inline");
-							//if objects's matches ref then append the ican statement to the table
-							if(f.Subconcept.localeCompare(ref)==0){
-								var tblRow = "<tr id=\"userdata\">" +"<td id=\"userdata\">" + f.ICanStatement  + "</td>" + "</tr>"
-								$(tblRow).appendTo("#userdata tbody");
-							//otherwise add a header and then ican statement
-						} else {
-								console.log(f.Subconcept);
-								//grab full subconcept name from key:value pairs
-								var sectionHeader= "<tr id=\"userdata\" style=\"background-color:#F26922;color:white;\">" +"<td id=\"userdata\"style=\"color:white;\">"  + subcons[f.Subconcept] + "</td>" + "</tr>"
-								var tblRow = "<tr id=\"userdata\">" +"<td id=\"userdata\">" + f.ICanStatement  + "</td>" + "</tr>"
-								$(sectionHeader).appendTo("#userdata tbody");
-								$(tblRow).appendTo("#userdata tbody");
-								//set the new reference
-								ref=f.Subconcept;
-							}
+				if( ( perSelect == per ) && ( prcSelect == prc ) && ( conSelect == con ) ){
+							
+					//if objects's matches ref then append the i-can statement to the table data
+					if (f.Subconcept.localeCompare(ref)==0) {
+						tableData += '<li>' + f.ICanStatement  + '</li>';
+						
+					//otherwise add a header + i-can statement
+					} else {
+//						console.log(f.Subconcept);
+						
+						if (tableData) {
+							tableData +='</ul></td></tr>';
 						}
+						
+						//grab full subconcept name from key:value pairs
+						tableData += '<tr>' + '<th>' + subcons[f.Subconcept] + '</th>';
+						tableData += '<td><ul><li>' + f.ICanStatement  + '</li>';
+												
+						//set the new reference
+						ref=f.Subconcept;
 					}
+
 				}
 			});
+			
+			tableData += '</ul></td></tr>';
+			
+			$('#userdata tbody').html(tableData);
+			$('#userdata caption').html( 'I can statements: ' + options[perSelect] + 's ' + options[prcSelect] + ' ' + options[conSelect]);
+			
+			//unhide table
+			$("#i-can-results").removeClass('hidden');
+			
+//			alert(tableData);
 		});
+		
 	}
 }
+
 window.onload = function(){
+
+	$('.dropdown a.box').click(function() {
+		$(this).closest('.dropdown').toggleClass('active');
+	});
+
 	$(".prbtn").click(function(){
-			//change main button name to selected option
-			document.getElementById("practice").innerHTML=($(this).html());
-			//grab the first class of the selected dropdown option
-			prcSelect=$(this).attr('class').split(' ')[0];
-			//invert button apperance
-			$(".practice").addClass("practiceinvert");
-			$(".practice").removeClass("practice");
-			//do this for every button for when user changes selection
-			fetchStatement();
-		});
+	
+		//change main button name to selected option and change appearance
+		$( '#practice' ).addClass( 'selected' ).children( '.box-content' ).html( $(this).html() );
+		$( this ).closest('.dropdown').removeClass('active');
+		
+		//grab the first class of the selected dropdown option
+		prcSelect = $(this).attr('data-value');
+
+		//do this for every button for when user changes selection
+		fetchStatement();
+	});
+	
 	$(".conbtn").click(function(){
-			//change button name to selected option
-			document.getElementById("concept").innerHTML=($(this).html());
-			//grab the first class of the selected dropdown option
-			conSelect=$(this).attr('class').split(' ')[0];
-			//invert button apperance
-			$(".concept").addClass("conceptinvert");
-			$(".concept").removeClass("concept");
-			//do this for every button for when user changes selection
-			fetchStatement();
-		});
+	
+		//change button name to selected option
+		$( '#concept' ).addClass( 'selected' ).children( '.box-content' ).html( $(this).html() );
+		$( this ).closest('.dropdown').removeClass('active');
+		
+		//grab the first class of the selected dropdown option
+		conSelect = $(this).attr('data-value');
+		
+		//do this for every button for when user changes selection
+		fetchStatement();
+	});
+	
 	$(".perbtn").click(function(){
-			//change button name to selected option
-			document.getElementById("perspective").innerHTML=($(this).html());
-			//grab the first class of the selected dropdown option
-			perSelect=$(this).attr('class').split(' ')[0];
-			//invert button apperance
-			$(".perspective").addClass("perspectiveinvert");
-			$(".perspective").removeClass("perspective");
-			//do this for every button for when user changes selection
-			fetchStatement();
-		});
-	};
+	
+		//change button name to selected option
+		$( '#perspective' ).addClass( 'selected' ).children( '.box-content' ).html( $(this).html() );
+		$( this ).closest('.dropdown').removeClass('active');
+		
+		//grab the first class of the selected dropdown option
+		perSelect = $(this).attr('data-value');
+		
+		//do this for every button for when user changes selection
+		fetchStatement();
+	});
+};
